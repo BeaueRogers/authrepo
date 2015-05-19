ac13cda48cd62b8f006fa38f8468d8d77fc9cd61
Beau Rogers <Beauerogers@gmail.com>
2015-05-19 15:59:09 -0500

    try 2

--- watch.sh
+++ watch.sh
@@ -29,7 +29,7 @@ src \
 
 # concat bower_components to public/lib
 if [ -d "bower_components" ]; then
-./node_modules/.bin/bowcat . -o public/lib -m
+./node_modules/.bin/bowcat -o public/lib
 fi
 
 # clean unneeded files
